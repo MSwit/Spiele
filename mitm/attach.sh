759c7db8416e3ae13aea7dfcbffbd44a49d0c742
Maik Switalski <Maik.Switalski@gmx.de>
2017-04-18 22:04:07 +0200

    mitm: first commit, added first attach and detach script

---
+++ mitm/attach.sh
@@ -0,0 +1,4 @@
+networksetup -setwebproxy Wi-Fi 127.0.0.1 8080
+networksetup -setsecurewebproxy Wi-Fi 127.0.0.1 8080
+networksetup -setwebproxy "AX88179 USB 3.0 to Gigabit Ethernet" 127.0.0.1 8080
+networksetup -setsecurewebproxy "AX88179 USB 3.0 to Gigabit Ethernet" 127.0.0.1 8080
---
+++ mitm/detach.sh
@@ -0,0 +1,4 @@
+networksetup -setwebproxystate Wi-Fi off
+networksetup -setsecurewebproxystate Wi-Fi off
+networksetup -setwebproxystate "AX88179 USB 3.0 to Gigabit Ethernet" off
+networksetup -setsecurewebproxystate "AX88179 USB 3.0 to Gigabit Ethernet" off
