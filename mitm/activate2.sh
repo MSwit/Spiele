9be5568627a60598025932bb506b15e35293be31
Maik Switalski <Maik.Switalski@gmx.de>
2017-05-05 15:19:26 +0200

    still working activate2.sh script. no proxy, but shared-init

--- mitm/activate2.sh
+++ mitm/activate2.sh
@@ -3,8 +3,10 @@
 
 sudo networksetup -setv6off Wi-Fi
 sudo networksetup -setv6off "AX88179 USB 3.0 to Gigabit Ethernet"
+#sudo networksetup -setv6off "bridge100"
 
 sudo pfctl -d
 sudo pfctl -F all
 sudo sysctl net.inet.ip.forwarding=1
+sudo sysctl -w net.inet.ip.fw.enable=1
 sudo pfctl -evf pf.conf
